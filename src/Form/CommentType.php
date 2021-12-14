feb51b5095e1a19bb6ed15cc9753234208f37eca
Chapelle Nathan <nathanchapelle76@gmail.com>
2021-12-14 17:23:26 +0100

    textareatype

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('rate', IntegerType::class)
-            ->add('comment', TextType::class);
+            ->add('comment', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
