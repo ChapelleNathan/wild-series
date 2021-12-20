1385d4e91f41f1bf605a252a001ba8d6806167f6
Nathan Chapelle <nathanchapelle76@gmail.com>
2021-12-20 10:43:39 +0100

    quest 22

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegistrationController extends AbstractController
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register")
@@ -38,7 +38,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_index');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
